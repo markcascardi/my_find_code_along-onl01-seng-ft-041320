061987053b8c50015e3c950cd60900e948d1e0a6
Mark Cascardi <mcascardi@gmail.com>
2020-04-16 04:21:08 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,10 +2,12 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  block_return_values = []
 
   while i < collection.length
-    block_return_values << yield(collection[i])
+    return collection[i] if yield(collection[i])
     i += 1
   end
 end
+
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
